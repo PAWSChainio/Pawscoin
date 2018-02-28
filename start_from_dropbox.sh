f4ce636dfb8c0c2e48473802b39d7a0496eaa398
Daniel Napierski <danielpnapierski@gmail.com>
2018-02-28 12:15:41 -0500

    clean

--- start_from_dropbox.sh
+++
@@ -1,1 +0,0 @@
-sudo ./src/pawscoind -debug -printtoconsole -datadir=/Users/danielnapierski/Dropbox/Pawscoin/macincloud/prodcoinbak/empty -connect=74.80.249.22
--- start_from_macincloud.sh
+++
@@ -1,1 +0,0 @@
-sudo ./src/pawscoind -debug -printtoconsole -datadir=/Users/danielnapierski/Coins/Pawschainio/macincloud -connect=74.80.249.22
